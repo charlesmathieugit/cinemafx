a2ccd77abf668d0bbb85931372c5fe57e866b9d4
kazumedu77 <charlesmathieu.champ@gmail.com>
2025-04-14 22:32:14 +0200

    cinema.sql

---
+++ src/main/java/bdd/cinemafx.sql
@@ -0,0 +1,58 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           8.0.30 - MySQL Community Server - GPL
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Listage de la structure de la base pour cinemafx
+CREATE DATABASE IF NOT EXISTS `cinemafx` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cinemafx`;
+
+-- Listage de la structure de table cinemafx. clients
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `birthdate` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `mail` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table cinemafx.clients : ~2 rows (environ)
+DELETE FROM `clients`;
+INSERT INTO `clients` (`id`, `name`, `adresse`, `email`, `birthdate`) VALUES
+	(3, 'toto', 'jvrv@ujeb', '24/03/2001', '454 avenue ideozp'),
+	(4, 'client1', 'client1@yahoo.fr', '27/04/1997', '155 avenue de Marianne'),
+	(5, 'tata', 'ezfr@reg', '12/05/2001', '25 av fcrf');
+
+-- Listage de la structure de table cinemafx. seances
+CREATE TABLE IF NOT EXISTS `seances` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `film` varchar(255) NOT NULL,
+  `salle` int NOT NULL,
+  `places` int NOT NULL,
+  `horaires` time NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table cinemafx.seances : ~0 rows (environ)
+DELETE FROM `seances`;
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
